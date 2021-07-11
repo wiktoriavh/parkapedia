8da7c6665c63676133bb7a365f927723bd888433
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-11 20:39:41 +0200

    :sparkles: create theme

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,15 +1,19 @@
-export const blue = "#1CBFE0"
-export const creme = "#f8f0e3"
-export const brown = "#5d5b58"
-export const purple = "#4b11af"
-export const green = "#8dc640"
-export const magenta = "#ff4975"
-export const white = "#000000"
-export const yellow = "#ffca0c"
+export const blue = '#1CBFE0';
+export const creme = '#f8f0e3';
+export const brown = '#5d5b58';
+export const purple = '#4b11af';
+export const green = '#8dc640';
+export const magenta = '#ff4975';
+export const white = '#000000';
+export const yellow = '#ffca0c';
 
-/**
- * poppins
- * 
- * <link rel="stylesheet" href="https://use.typekit.net/zao8law.css">
- * 
- */
+export const parkasaurusTheme = {
+  blue: '#1CBFE0',
+  creme: '#f8f0e3',
+  brown: '#5d5b58',
+  purple: '#4b11af',
+  green: '#8dc640',
+  magenta: '#ff4975',
+  white: '#000000',
+  yellow: '#ffca0c',
+};
