7d92b31ec426652659fe60c9892c83ce95848d5e
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-11 13:36:21 +0200

    :lipstick: color variables
    the colour names are not very good

---
+++ utils/colors.ts
@@ -0,0 +1,15 @@
+export const blue = "#1CBFE0"
+export const creme = "#f8f0e3"
+export const brown = "#5d5b58"
+export const purple = "#4b11af"
+export const green = "#8dc640"
+export const magenta = "#ff4975"
+export const white = "#000000"
+export const yellow = "#ffca0c"
+
+/**
+ * poppins
+ * 
+ * <link rel="stylesheet" href="https://use.typekit.net/zao8law.css">
+ * 
+ */
