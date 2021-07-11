8f6d2591734c532586a5360b5b88f1bf0c240716
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-11 13:35:43 +0200

    :wrench: i18n configs && add jss and clsx

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const { i18n } = require('./next-i18next.config');
+
 module.exports = {
+  i18n,
   reactStrictMode: true,
   eslint: {
     // Warning: Dangerously allow production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-}
+};
