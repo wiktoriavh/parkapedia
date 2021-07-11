3a1e137f47957b7dd85a0a4c22c6283166c41406
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-11 13:38:10 +0200

    :sparkles: custom paragraphs

---
+++ components/typography/Paragraph.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  paragraph: {
+    fontFamily: 'Poppins',
+    fontWeight: 400,
+    fontSize: 20,
+    lineHeight: 35,
+    marginLeft: 20,
+  },
+});
+
+export const Paragraph = ({ children }: { children: ReactNode }) => {
+  const classes = useStyles();
+
+  return <p className={classes.paragraph}>{children}</p>;
+};
