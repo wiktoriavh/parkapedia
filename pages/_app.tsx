eeafd2d8714cff5b58d341a4354fb017e58e8f17
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-11 13:39:08 +0200

    :sparkles: import Main Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Main } from '../components/layouts/Main';
+import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Main />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+// export default MyApp;
+
+export default appWithTranslation(MyApp);
