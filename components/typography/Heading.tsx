4066fec97cef770df822d934ad4c4d616960fe83
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-12 20:40:49 +0200

    :bulb: comment out not declaed styles of headings

--- components/typography/Heading.tsx
+++ components/typography/Heading.tsx
@@ -11,14 +11,14 @@ type ComponentVariantTypes =
   | 1
   | 'h2'
   | 2
-  | 'h3'
-  | 3
-  | 'h4'
-  | 4
-  | 'h5'
-  | 5
-  | 'h6'
-  | 6;
+  // | 'h3'
+  // | 3
+  // | 'h4'
+  // | 4
+  // | 'h5'
+  // | 5
+  // | 'h6'
+  // | 6;
 
 type HeadingProps = {
   variant?: ComponentVariantTypes;
