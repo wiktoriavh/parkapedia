77dc104257ee6878a9a6fcd70b8f61bfc574e6e5
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-13 22:11:23 +0200

    :sparkles: Button component

---
+++ components/styled/Button.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+import { HTMLAttributes, ReactNode } from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles<string, unknown, { [key: string]: string }>(
+  (theme) => ({
+    button: {
+      fontFamily: 'gooddog-new',
+      fontSize: 20,
+      backgroundColor: theme.yellow,
+      border: 0,
+      borderRadius: 8,
+      padding: '5px 20px',
+      borderBottom: '3px solid #F4BF01',
+      color: 'rgba(0,0,0,0.7)',
+
+      '&:hover': {
+        backgroundColor: '#F9C700',
+      },
+
+      '&:active': {
+        backgroundColor: '#F4BF01',
+      },
+    },
+  })
+);
+
+type ButtonProps = {
+  children: ReactNode;
+} & HTMLAttributes<HTMLButtonElement>;
+
+export function Button({ children, className, ...other }: ButtonProps): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <button className={clsx(classes.button, className)} {...other}>
+      {children}
+    </button>
+  );
+}
