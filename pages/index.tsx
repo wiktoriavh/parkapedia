b363a0b11d9db8f4b9c03b0981434b7d152f29a7
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-15 22:15:22 +0200

    :alembic: testing table and keyvaluepair

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,27 @@
 import type { UserConfig } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { Box } from '../components/layouts/Box';
 
 import { HeadingSection } from '../components/layouts/HeadingSection';
 import { Button } from '../components/styled/Button';
+import { KeyValuePair } from '../components/table/KeyValuePair';
+import { SimpleTable } from '../components/table/SimpleTable';
 import type { Locale } from '../utils/i18nextTypes';
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <HeadingSection title="I am a Heading, yo" />
-      <Button>I am a Button</Button>
+      <Box col={6}>
+        <SimpleTable
+          data={[
+            ['Dino', 'tier 2', 'herd', 'rainforest'],
+            ['Dino 2', 'tier 1', 'mate', 'swamp'],
+          ]}
+        />
+        <KeyValuePair typeColor="blue" label={'Type Species'} value={150} />
+        <KeyValuePair typeColor="yellow" label={'Type Species'} value={150} />
+      </Box>
     </div>
   );
 }
