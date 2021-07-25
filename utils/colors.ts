d99b63fc1b3eb1daffea5ee6e08e0c293e724fe2
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-25 14:51:46 +0200

    ✨ add more colours

--- utils/colors.ts
+++ utils/colors.ts
@@ -16,4 +16,13 @@ export const parkasaurusTheme = {
   magenta: '#ff4975',
   white: '#000000',
   yellow: '#ffca0c',
+  savannah: "#e95816",
+  prairie: "#f39c16",
+  desert: "#ffe00d",
+  taiga: "#26AE60",
+  forest: "#8DC640",
+  rainforest: "#008790",
+  swamp: "#2275A9",
+  alpine: "#00BE95",
+  tundra: "#63D4F9"
 };
