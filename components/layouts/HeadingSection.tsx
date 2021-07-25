bb56ce215a8ad323a98e5ccce00fb88b40836e42
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-25 14:38:32 +0200

    🔥 remove wrong import

--- components/layouts/HeadingSection.tsx
+++ components/layouts/HeadingSection.tsx
@@ -1,4 +1,4 @@
-import { createUseStyles, ThemeProvider } from 'react-jss';
+import { createUseStyles } from 'react-jss';
 
 import { Heading } from '../typography/Heading';
 import { Box } from './Box';
