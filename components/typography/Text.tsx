c8208dbd290d2f1ffa44f6d10866e23772ff0f1d
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-07-25 14:53:23 +0200

    🐛 fix type bug

--- components/typography/Text.tsx
+++ components/typography/Text.tsx
@@ -1,23 +1,50 @@
-import type { HTMLAttributes, ReactNode } from 'react';
-import { createElement, forwardRef } from 'react';
+import type { ReactNode } from 'react';
+import { forwardRef } from 'react';
 
-type TextProps = {
+type TextProps<T extends keyof JSX.IntrinsicElements | number> = {
   children: ReactNode;
-  component: string | number;
-} & HTMLAttributes<HTMLElement>;
+  component: T;
+} & JSX.IntrinsicElements[T extends number ? 'h1' : T];
 
-export type TextRef = HTMLElement;
+const defaultComponentMapping = {
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+  h4: 'h4',
+  h5: 'h5',
+  h6: 'h6',
+  p: 'p',
+  span: 'span',
+  strong: 'strong',
+  em: 'em',
+  b: 'b',
+  i: 'i',
+  u: 'u',
+  s: 's',
+  sub: 'sub',
+  sup: 'sup',
+} as const;
 
-export const Text = forwardRef<TextRef, TextProps>(
-  ({ children, component, ...other }, ref): JSX.Element => {
-    let componentType = '';
+declare function GenericText<Tag extends keyof typeof defaultComponentMapping | number>(
+  props: TextProps<Tag>
+): JSX.Element;
 
-    if (typeof component === 'number') {
-      componentType = `h${component}`;
-    } else {
-      componentType = component;
-    }
+function mapComponent<T extends Record<string, keyof JSX.IntrinsicElements>>(
+  mapping: T,
+  input: string | number
+): T[keyof T] {
+  const key = typeof input === 'number' ? `h${~~input}` : input;
+  if (!(key in mapping)) throw 'Illegal element'; // Check for invalid props that snuck past type checking? I dunno
+  return mapping[key as keyof typeof mapping];
+}
 
-    return createElement(componentType, { ref, ...other }, children);
-  }
-);
+export const Text = forwardRef<HTMLElement, TextProps<any>>((props, ref): JSX.Element => {
+  const { children, component, ...other } = props;
+  const Component = mapComponent(defaultComponentMapping, component);
+
+  return (
+    <Component ref={ref} {...other}>
+      {children}
+    </Component>
+  );
+}) as typeof GenericText;
