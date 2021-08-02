b864b4f0d35aa622a52ec05db24dea047e9ca08d
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-08-02 21:04:37 +0200

    ✨ get data for all dinosaurs

--- pages/dinosaurs/index.tsx
+++ pages/dinosaurs/index.tsx
@@ -1,3 +1,45 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { getAllPosts, getPostBySlug } from '../../utils/routes';
+import type { Locale } from '../../utils/i18nextTypes';
+
 export default function Dinosaurs(): JSX.Element {
   return <span>Dinosaurs</span>;
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts('dinosaurs');
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          dinosaur: post.name,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({
+  params,
+  locale,
+}: {
+  params: Record<string, string>;
+  locale: Locale;
+}) {
+  const post = getPostBySlug(params.slug, 'dinosaurs');
+
+  if (!locale) {
+    throw new Error('locale is undefined.');
+  }
+
+  return {
+    props: {
+      post: {
+        ...post,
+      },
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
 }
