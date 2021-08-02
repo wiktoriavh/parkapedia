e6e03399445bbc3941ca260b2de9fd9fbebeb315
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-08-02 20:50:00 +0200

    🏷️ types for Dinosaur Object

---
+++ lib/DinosaurType.ts
@@ -0,0 +1,39 @@
+export type Welcome = {
+  name: string;
+  class: string;
+  image: string;
+  dino_information: DinoInformation;
+  affect: Affect;
+  exhibit: Exhibit;
+  egg_items: EggItems;
+  size: string;
+  food: string;
+  tier: number;
+  herd: boolean;
+};
+
+export type Affect = {
+  appeal: number;
+  scariness: number;
+  damage: number;
+};
+
+export type DinoInformation = {
+  type_species: string;
+  length: number;
+  weight: number;
+  height: number;
+  period: string;
+};
+
+export type EggItems = {
+  theropoda_footprint: number;
+  theropoda_skull: number;
+  epic_gem: number;
+};
+
+export type Exhibit = {
+  tiles: string;
+  biome: string;
+  exhibitSize: number;
+};
