6a27a777224c0ff88909b022ee728f45a123f5db
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-08-08 12:17:12 +0200

    🔨 display list of dinosaurs

--- pages/dinosaurs/index.tsx
+++ pages/dinosaurs/index.tsx
@@ -1,34 +1,29 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { getAllPosts, getPostBySlug } from '../../utils/routes';
 import type { Locale } from '../../utils/i18nextTypes';
+import Link from 'next/Link';
 
-export default function Dinosaurs(): JSX.Element {
-  return <span>Dinosaurs</span>;
-}
-
-export async function getStaticPaths() {
-  const posts = getAllPosts('dinosaurs');
-
-  return {
-    paths: posts.map((post) => {
-      return {
-        params: {
-          dinosaur: post.name,
-        },
-      };
-    }),
-    fallback: false,
-  };
+export default function Dinosaurs(props: Record<string, string[]>): JSX.Element {
+  return (
+    <ul>
+      {props.slugs.map((slug) => (
+        <li>
+          <Link href={`/dinosaurs/${slug}`}>
+            <a>{slug}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export async function getStaticProps({
-  params,
   locale,
 }: {
   params: Record<string, string>;
   locale: Locale;
 }) {
-  const post = getPostBySlug(params.slug, 'dinosaurs');
+  const posts = getAllPosts('dinosaurs');
 
   if (!locale) {
     throw new Error('locale is undefined.');
@@ -36,9 +31,7 @@ export async function getStaticProps({
 
   return {
     props: {
-      post: {
-        ...post,
-      },
+      slugs: posts.map((post) => post.name),
       ...(await serverSideTranslations(locale, ['common'])),
     },
   };
